092f0b4636951d397e2eb6085ec9e1f47e82baff
Alex Rogov <alexrogov@quintagroup.org>
2023-08-29 09:57:47 +0300

    fix: resolve path for files containing space in the filename

--- src/astro-babel-inline-scripts.ts
+++ src/astro-babel-inline-scripts.ts
@@ -1,52 +1,56 @@
-import type { AstroIntegration } from 'astro'
-import { readFileSync, writeFileSync } from 'fs'
-import path from 'path'
-import babel, { type TransformOptions } from '@babel/core'
+import type { AstroIntegration } from "astro";
+import { readFileSync, writeFileSync } from "fs";
+import babel, { type TransformOptions } from "@babel/core";
+import { fileURLToPath } from "node:url";
 
 export default (options: TransformOptions = {}): AstroIntegration => {
   return {
-    name: 'babel-inline-scripts',
+    name: "babel-inline-scripts",
     hooks: {
-      'astro:build:done': ({ routes, dir }) => {
-        const scriptTagRegex = /<script.*?>([\s\S]*?)<\/script>/gim
+      "astro:build:done": ({ routes }) => {
+        const scriptTagRegex = /<script.*?>([\s\S]*?)<\/script>/gim;
 
-        const stripScriptTags = (htmlString: string) => htmlString.replace(scriptTagRegex, '$1')
+        const stripScriptTags = (htmlString: string) =>
+          htmlString.replace(scriptTagRegex, "$1");
 
-        const HTMLPaths = routes.map(({ distURL }) => distURL?.pathname).filter(Boolean) as Array<string>
+        const HTMLURLs = routes
+          .map(({ distURL }) => distURL?.href)
+          .filter(Boolean) as Array<string>;
 
-        const dirName = path.basename(dir.pathname)
+        HTMLURLs.forEach((HTMLURL) => {
+          const HTMLPath = fileURLToPath(HTMLURL);
 
-        HTMLPaths.forEach((HTMLPath) => {
-          const HTMLPathRelative = path.resolve(dirName + HTMLPath.split(dirName)[1])
+          const byteContent = readFileSync(HTMLPath);
+          const stringContent = String(byteContent);
 
-          const byteContent = readFileSync(HTMLPathRelative)
-          const stringContent = String(byteContent)
-
-          const scriptBodies: Array<string> = []
-          const transpiledScriptBodies: Array<string> = []
-          let replacedStringContent = stringContent
+          const scriptBodies: Array<string> = [];
+          const transpiledScriptBodies: Array<string> = [];
+          let replacedStringContent = stringContent;
 
           replacedStringContent.match(scriptTagRegex)?.forEach((script) => {
-            const scriptBody = stripScriptTags(script)
-            scriptBodies.push(scriptBody)
+            const scriptBody = stripScriptTags(script);
+            scriptBodies.push(scriptBody);
 
-            const transpiledResult = babel.transform(scriptBody, options)
-            const transpiledScript = transpiledResult?.code
+            const transpiledResult = babel.transform(scriptBody, options);
+            const transpiledScript = transpiledResult?.code;
 
             if (transpiledScript) {
-              transpiledScriptBodies.push(transpiledScript)
+              transpiledScriptBodies.push(transpiledScript);
             }
-          })
+          });
 
           transpiledScriptBodies.forEach((transpiledScriptBody, index) => {
-            replacedStringContent = replacedStringContent.replace(scriptBodies[index], transpiledScriptBody)
-          })
+            replacedStringContent = replacedStringContent.replace(
+              scriptBodies[index],
+              transpiledScriptBody
+            );
+          });
 
-          const replacedByteContent = Buffer.from(replacedStringContent)
+          const replacedByteContent = Buffer.from(replacedStringContent);
 
-          writeFileSync(HTMLPathRelative, replacedByteContent)
-        })
+          writeFileSync(HTMLPath, replacedByteContent);
+        });
       },
     },
-  }
-}
+  };
+};
